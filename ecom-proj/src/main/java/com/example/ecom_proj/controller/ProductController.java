71e98a0179e7cfd25311054bd544b7987d6df875
SKY <aakashkumargupta.cse27@jecrc.ac.in>
2025-04-19 16:36:47 +0530

    FRONTEND+database

--- ecom-proj/src/main/java/com/example/ecom_proj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/example/ecom_proj/controller/ProductController.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLOutput;
 import java.util.List;
 
 @RestController
@@ -33,5 +35,22 @@ public class ProductController {
         else
             return new ResponseEntity<>( HttpStatus.NOT_FOUND);
     }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile){
+        try {
+            System.out.println(product);
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+
+        }
+        catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+
+
+    }
+
 
 }
