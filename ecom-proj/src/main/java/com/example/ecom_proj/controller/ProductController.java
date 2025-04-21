78fe7c41457f8b123140f627b837c86faf1a92c1
SKY <aakashkumargupta.cse27@jecrc.ac.in>
2025-04-21 17:41:52 +0530

    FRONTEND+database

--- ecom-proj/src/main/java/com/example/ecom_proj/controller/ProductController.java
+++ ecom-proj/src/main/java/com/example/ecom_proj/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.example.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -48,7 +49,17 @@ public class ProductController {
             return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
+    }
 
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+
+        Product product = service.getProductById(productId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile);
 
     }
 
--- ecom-proj/src/main/java/com/example/ecom_proj/model/Product.java
+++ ecom-proj/src/main/java/com/example/ecom_proj/model/Product.java
@@ -19,7 +19,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
     private String name;
-    private String desc;
+    private String description;
     private String brand;
     private BigDecimal price;
     private String category;
@@ -27,8 +27,9 @@ public class Product {
 
     // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")  NOW TAKE CARE BY FRONTEND _
     private Date releaseDate;
-//    private boolean available;
-//    private int quantity;
+    private boolean productAvailable;
+
+    private int stockQuantity;
     private String imageName;
     private String imageType;
     @Lob
